8d0462d25f41ab84e4fc62873e0f2d768064b8be
Priyanka-1505 <124334301+Priyanka-1505@users.noreply.github.com>
2025-03-22 11:42:47 +0000

    conversions

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = "33"
+
+// console.log(typeof score)
+
+let score_int = Number(score)
+// console.log(typeof score)
+// console.log(typeof score_int)
+// console.log(score_int)
+
+let login = true
+let convert_login = Boolean(login)
+// console.log(typeof login)
+// console.log(typeof convert_login)
+// console.log(convert_login)
+
+
+//*****************Operations ********************//
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+// console.log("1"+ 2);
+// console.log(1 +"2");
+// console.log("1"+ 2 + 2);
+// console.log(1 + 2 + "2");
+
+// ****Prefix****
+let x = 4
+let y = x++
+ console.log(`x:${x}, y:${y}`)
+// ****Suffix****
+let x1 = 4
+let y1 = ++x1
+console.log(`x:${x1}, y:${y1}`)
